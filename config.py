6d00a6add637c81153d75ee854284f25f4cf3a94
Matthew Emond <matthew.emond@ucf.edu>
2015-11-09 10:48:42 -0500

    Removed config.py from repo

--- config.py
+++
@@ -1,8 +0,0 @@
-server_ip = "0.0.0.0"
-servername = 'http://' + server_ip + ':8080'
-
-COURSE_TEMPLATE = '751378'
-
-ENDPOINT = servername + '/endpoint'
-API_URL = '***REMOVED***'
-API_KEY = '***REMOVED***'
