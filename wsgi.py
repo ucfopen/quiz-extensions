71305d297489523aec1bb25bc9242a547e083bbe
Shea <shea.silverman@ucf.edu>
2016-08-23 10:55:48 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+activate_this = '/var/www/quiz-extensions-license/env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 import sys
 sys.path.insert(0,"/var/www/quiz-extensions-license/")
 
