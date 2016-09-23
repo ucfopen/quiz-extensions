ddc3cc12bab28eca679b0da88d5d84eb90a815a9
Matthew Emond <me@ucf.edu>
2016-09-23 16:49:36 -0400

    Added last_updated dates to models. Changed some table names

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     canvas_id = db.Column(db.Integer, unique=True)
     created_date = db.Column(db.DateTime, server_default=db.func.now())
@@ -13,10 +13,15 @@ class User(db.Model):
         backref='user',
         lazy='dynamic'
     )
+    last_updated_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        onupdate=db.func.now()
+    )
     sis_id = db.Column(db.String(50))
     sortable_name = db.Column(db.String(250))
 
-    def __init__(self, sortable_name, canvas_id, sis_id):
+    def __init__(self, canvas_id, sis_id=None, sortable_name=None):
         self.canvas_id = canvas_id
         self.sis_id = sis_id
         self.sortable_name = sortable_name
@@ -26,7 +31,7 @@ class User(db.Model):
 
 
 class Course(db.Model):
-    __tablename__ = 'courses'
+    __tablename__ = 'course'
     id = db.Column(db.Integer, primary_key=True)
     canvas_id = db.Column(db.Integer)
     course_name = db.Column(db.String(250))
@@ -36,13 +41,18 @@ class Course(db.Model):
         backref='course',
         lazy='dynamic'
     )
+    last_updated_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        onupdate=db.func.now()
+    )
     quizzes = db.relationship(
         'Quiz',
         backref='course',
         lazy='dynamic'
     )
 
-    def __init__(self, canvas_id, course_name):
+    def __init__(self, canvas_id, course_name=None):
         self.canvas_id = canvas_id
         self.course_name = course_name
 
@@ -51,12 +61,17 @@ class Course(db.Model):
 
 
 class Extension(db.Model):
-    __tablename__ = 'extensions'
+    __tablename__ = 'extension'
     id = db.Column(db.Integer, primary_key=True)
-    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('course.canvas_id'))
     created_date = db.Column(db.DateTime, server_default=db.func.now())
+    last_updated_date = db.Column(
+        db.DateTime,
+        server_default=db.func.now(),
+        onupdate=db.func.now()
+    )
     percent = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.canvas_id'))
     __table_args__ = (
         db.CheckConstraint(percent >= 100, name='check_percent_greater_than_100'),
     )
@@ -68,10 +83,11 @@ class Extension(db.Model):
 
 
 class Quiz(db.Model):
-    __tablename__ = 'quizzes'
+    __tablename__ = 'quiz'
     id = db.Column(db.Integer, primary_key=True)
     canvas_id = db.Column(db.Integer)
-    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('course.canvas_id'))
+    created_date = db.Column(db.DateTime, server_default=db.func.now())
     last_updated_date = db.Column(
         db.DateTime,
         server_default=db.func.now(),
@@ -79,7 +95,7 @@ class Quiz(db.Model):
     )
     title = db.Column(db.String(250))
 
-    def __init__(self, canvas_id, course_id, title):
+    def __init__(self, canvas_id, course_id, title=None):
         self.canvas_id = canvas_id
         self.course_id = course_id
         self.title = title
