5e862c9a10d9d620af09ff35d867bb9052185f68
Matthew Emond <me@ucf.edu>
2017-03-09 16:32:54 -0500

    Removed debug from config. Use the FLASK_DEBUG environment variable instead.

--- views.py
+++ views.py
@@ -21,7 +21,6 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
-app.debug = config.DEBUG
 app.secret_key = config.SECRET_KEY
 
 db.init_app(app)
