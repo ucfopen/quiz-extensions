186d14a836a4076807759c042c4b740d5929b6b2
Luke Cullen <luke.cullen@ucf.edu>
2026-04-08 09:23:53 -0400

    run isort

--- lti/wsgi.py
+++ lti/wsgi.py
@@ -1,8 +1,7 @@
 import sys
 
-from virtualenv import activation
-
 from views import app as application
+from virtualenv import activation
 
 activate_this = f"{activation.python.__path__[0]}/activate_this.py"
 exec(open(activate_this).read(), dict(__file__=activate_this))
